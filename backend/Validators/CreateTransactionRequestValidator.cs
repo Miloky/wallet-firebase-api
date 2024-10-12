ef6f7c0fd57b6784f6f158e97c6e9721cfc660e3
Miloky <monmorti@gmail.com>
2024-10-12 23:34:46 +0300

    install fluent validation

---
+++ backend/Validators/CreateTransactionRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Wallet.Firebase.Api.Models.Requests;
+
+namespace Wallet.Firebase.Api.Validators;
+
+public class CreateTransactionRequestValidator: AbstractValidator<CreateTransactionRequest>
+{
+    public CreateTransactionRequestValidator()
+    {
+        RuleFor(x => x.Amount).GreaterThan(0);
+        RuleFor(x => x.Description).NotEmpty();
+        RuleFor(x => x.Type).IsInEnum();
+    }
+}
